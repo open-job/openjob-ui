278acb5ec595e2adcc32784832e954df4c269586
JasonYHZ <coderyu2018@gmail.com>
2022-11-02 13:16:09 +0800

    chore: 🔨 enable emoji in the commit msg

--- commitlint.config.js
+++ commitlint.config.js
@@ -65,6 +65,50 @@ module.exports = {
     allowEmptyIssuePrefixs: false,
     allowCustomIssuePrefixs: false,
 
+    useEmoji: true,
+    emojiAlign: 'center',
+    types: [
+      { value: 'feat', name: 'feat:     ✨  A new feature', emoji: ':sparkles:' },
+      { value: 'fix', name: 'fix:      🐛  A bug fix', emoji: ':bug:' },
+      { value: 'docs', name: 'docs:     📝  Documentation only changes', emoji: ':memo:' },
+      {
+        value: 'style',
+        name: 'style:    💄  Changes that do not affect the meaning of the code',
+        emoji: '💄',
+      },
+      {
+        value: 'refactor',
+        name: 'refactor: ♻️   A code change that neither fixes a bug nor adds a feature',
+        emoji: '♻️',
+      },
+      {
+        value: 'perf',
+        name: 'perf:     ⚡️  A code change that improves performance',
+        emoji: '⚡️',
+      },
+      {
+        value: 'test',
+        name: 'test:     ✅  Adding missing tests or correcting existing tests',
+        emoji: '✅',
+      },
+      {
+        value: 'build',
+        name: 'build:    📦️   Changes that affect the build system or external dependencies',
+        emoji: '📦️',
+      },
+      {
+        value: 'ci',
+        name: 'ci:       🎡  Changes to our CI configuration files and scripts',
+        emoji: '🎡',
+      },
+      {
+        value: 'chore',
+        name: "chore:    🔨  Other changes that don't modify src or test files",
+        emoji: '🔨',
+      },
+      { value: 'revert', name: 'revert:   ⏪️  Reverts a previous commit', emoji: '⏪️' },
+    ],
+
     // English
     typesAppend: [
       { value: 'wip', name: 'wip:      work in process' },
