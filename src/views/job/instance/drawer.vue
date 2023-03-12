668fb54e9d27062b07a97ad687dca77ffd3144cf
stelin <794774870@qq.com>
2023-03-12 20:29:06 +0800

    :bug:fixed bug

--- src/views/job/instance/drawer.vue
+++ src/views/job/instance/drawer.vue
@@ -1,7 +1,16 @@
 <template>
-  <el-drawer v-model="state.drawer.isShow" direction="rtl" size="80%">
+  <el-drawer v-model="state.drawer.isShow" direction="rtl" size="80%" @close="onDrawerClose()">
     <template #header>
-      <h4>新增定时任务</h4>
+      <el-descriptions style="margin-top: 10px;" column="6">
+        <el-descriptions-item label="工作节点:" width="260px" align="left">192.168.31.136:25531
+        </el-descriptions-item>
+        <el-descriptions-item label="开始时间:" width="260px" align="left">2023-12-12 15:12:12
+        </el-descriptions-item>
+        <el-descriptions-item label="完成时间:" width="260px" align="left">2023-12-12 15:12:12
+        </el-descriptions-item>
+        <el-descriptions-item label="任务状态:" width="260px" align="left">成功
+        </el-descriptions-item>
+      </el-descriptions>
     </template>
     <template #default>
       <div class="system-role-dialog-container" style="padding: 10px;height: 100%">
@@ -30,6 +39,7 @@ import {ElMessage} from 'element-plus'
 import {useI18n} from "vue-i18n";
 import {defineAsyncComponent, reactive} from "vue";
 import {useJobInstanceApi} from "/@/api/job";
+
 const MonacoEditor = defineAsyncComponent(() => import('/@/components/editor/monaco.vue'));
 
 const {t} = useI18n();
@@ -40,7 +50,7 @@ const jobInstanceApi = useJobInstanceApi();
 const state = reactive({
   editor: {
     editorStyle: 'width: 100%;height: 100%;',
-    language: 'plaintext',
+    language: 'shell',
     value: '',
   },
   drawer: {
@@ -48,23 +58,68 @@ const state = reactive({
   },
 })
 
+const loadingState = reactive({
+  timerId: 0,
+  time: 0,
+  counter: 0,
+});
+
 const openDrawer = async (row: RowJobInstanceType) => {
   state.drawer.isShow = true;
   state.editor.value = '';
+  loadingState.time = 0;
+  await loadingLog(row, false);
+}
 
+const loadingLog = async (row: RowJobInstanceType, isTimer: boolean) => {
+  if (loadingState.counter > 0 && loadingState.counter % 60 == 0 && isTimer) {
+    state.editor.value += '\n';
+  }
+
+  let size = 100;
   let data = await jobInstanceApi.getProcessorList({
     jobInstanceId: row.id,
     executeType: row.executeType,
-    time: 0,
-    page: 20,
+    time: loadingState.time,
+    page: size,
   });
 
   data.list.forEach(function (line: string) {
     state.editor.value += line + '\n'
   });
+
+  if (data.time > 0) {
+    loadingState.time = data.time;
+  }
+
+  if (data.complete == 1){
+    return;
+  }
+
+  if (data.list.length > 0) {
+    setTimeout(async () => {
+      await loadingLog(row, false)
+    }, 500);
+    return;
+  }
+
+  loadingState.timerId = setInterval(() => {
+    state.editor.value += '.';
+    loadingState.counter += 1;
+    if (loadingState.counter % 6 == 0) {
+      clearInterval(loadingState.timerId);
+      loadingLog(row, true);
+    }
+  }, 500);
 }
+
+const onDrawerClose = () => {
+  clearInterval(loadingState.timerId);
+}
+
 const cancelClick = () => {
-  state.drawer.isShow = false
+  state.drawer.isShow = false;
+  clearInterval(loadingState.timerId);
 }
 const confirmClick = async () => {
   ElMessage.success('更新成功');
